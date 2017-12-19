9b6f626576aa1084a18742a7402ff4f53748ddcb
inta.kovalenko <inta.kovalenko@accenture.com>
2017-12-19 18:45:13 +0200

    Modified SpeedSupport interface and Car class. Reworked the demonstration.

--- javaoop/src/main/java/bootcamp/Car.java
+++ javaoop/src/main/java/bootcamp/Car.java
@@ -16,20 +16,27 @@ public class Car implements Printable, SpeedSupport {
     public double accelerationTime;
     // Speed of the car measured in km/h
     public int speed;
+    // State of clutch
+    public boolean isClutchPressed;
+    // Gear number
+    public int gearNumber;
+
 
     // Default constructor
     public Car() {}
 
     // Full constructor
     public Car(String brand, String model, int year, String fuelType,
-               int horsepower, double accelerationTime, int speed) {
+               int horsepower, double accelerationTime) {
         this.brand = brand;
         this.model = model;
         this.year = year;
         this.fuelType = fuelType;
         this.horsepower = horsepower;
         this.accelerationTime = accelerationTime;
-        this.speed = speed;
+        this.speed = 0;
+        this.isClutchPressed = false;
+        this.gearNumber = 0;
     }
 
     // Prints an essential information of a car
@@ -43,19 +50,38 @@ public class Car implements Printable, SpeedSupport {
     }
 
     // Increments the speed of the car by 1 unit
-    public void goFaster(){
-        this.speed = this.speed + 1;
-        System.out.println("Increased the speed of a car " + this.brand + " " + this.model);
+    public void goFaster() throws Exception {
+        // If clutch pedal is not pressed, then the speed can be manually increased
+        if (!isClutchPressed) {
+            this.speed = this.speed + 1;
+            System.out.println("Increased the speed of a car " + this.brand + " " + this.model);
+        } else {
+            throw new Exception();
+        }
     }
 
     // Decrements the speed of the car by 1 unit
-    public void goSlower(){
-        this.speed = this.speed - 1;
-        System.out.println("Decreased the speed of a car " + this.brand + " " + this.model);
+    public void goSlower() throws Exception {
+        // If clutch pedal is pressed, then speed can be manually decreesed
+        if (isClutchPressed) {
+            this.speed = this.speed - 1;
+            System.out.println("Decreased the speed of a car " + this.brand + " " + this.model);
+        } else {
+            throw new Exception();
+        }
+    }
+
+    // Shifts the gear of a car
+    public void shiftGear(int gearNumber) throws Exception {
+        if (isClutchPressed) {
+            this.gearNumber = gearNumber;
+        } else {
+            throw new Exception();
+        }
     }
 
     // Prints out the speed of the car
-    public void printSpeed(){
+    public void printSpeed() {
         System.out.println("Car's " + this.brand + " " + this.model + " current speed is " + this.speed + " km/h");
     }
 }
