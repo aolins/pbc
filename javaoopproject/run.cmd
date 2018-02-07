63b20f0d4f6f122d774284c900371d896a92b474
Andrejs Olins <andrejs.olins@restore.eu>
2018-02-07 09:00:24 +0100

    oop project

---
+++ javaoopproject/run.cmd
@@ -0,0 +1,5 @@
+echo Hello world
+echo Now the JAVA is about to run
+javac src/bootcamp/OopPrinciples.java
+java -cp ./src bootcamp.OopPrinciples
+PAUSE
---
+++ javaoopproject/run.sh
@@ -0,0 +1,6 @@
+echo Hello world
+echo Now the JAVA is about to run
+#!/bin/bash
+javac src/bootcamp/OopPrinciples.java
+java -cp ./src bootcamp.OopPrinciples
+read
