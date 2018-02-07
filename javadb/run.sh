9434aa526f9d2246c33f9d34256362e0b9cab3e9
Andrejs Olins <andrejs.olins@restore.eu>
2018-02-07 08:59:54 +0100

    clean

--- javadb/run.sh
+++
@@ -1,2 +0,0 @@
-echo Hello world
-echo Now the JAVA is about to run
--- javaoop/run.sh
+++
@@ -1,6 +0,0 @@
-echo Hello world
-echo Now the JAVA is about to run
-#!/bin/bash
-javac src/main/java/bootcamp/OopPrinciples.java
-java -cp ./src/main/java bootcamp.OopPrinciples
-read
